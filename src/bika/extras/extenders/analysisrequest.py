099836c5695b24de7491ff875140ccbb5d49db69
Luthando Ndlela <104898641+luthando5@users.noreply.github.com>
2023-05-18 13:23:02 +0200

    Lims 361 sample matrices (#2)
    * Added extenders folder and new ar attribute
    
    * Now able to edit sample matrix
    
    * Changed edit permissions to that of a batch

---
+++ src/bika/extras/extenders/analysisrequest.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from Products.CMFCore.permissions import View
+from archetypes.schemaextender.interfaces import IBrowserLayerAwareExtender
+from archetypes.schemaextender.interfaces import ISchemaExtender
+from zope.component import adapts
+from zope.interface import implementer
+
+from .fields import ExtUIDReferenceField
+from bika.lims.interfaces import IAnalysisRequest
+from bika.lims.permissions import FieldEditBatch
+from bika.lims.browser.widgets import ReferenceWidget
+from bika.extras import _
+from bika.extras.interfaces import IBikaExtrasLayer
+
+sample_matrix_field = ExtUIDReferenceField(
+    'SampleMatrix',
+    required=0,
+    allowed_types=('SampleMatrix',),
+    mode="rw",
+    write_permission=FieldEditBatch,
+    read_permission=View,
+    widget=ReferenceWidget(
+        label=_("Sample Matrix"),
+        render_own_label=True,
+        visible={
+            'add': 'edit',
+            'secondary': 'disabled',
+        },
+        catalog_name='senaite_catalog_setup',
+        base_query={"is_active": True,
+                    "sort_on": "sortable_title",
+                    "sort_order": "ascending"},
+        showOn=True,
+    ),
+)
+
+@implementer(ISchemaExtender, IBrowserLayerAwareExtender)
+class AnalysisRequestSchemaExtender(object):
+    adapts(IAnalysisRequest)
+    layer = IBikaExtrasLayer
+
+    fields = [
+        sample_matrix_field,
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getOrder(self, schematas):
+        return schematas
+
+    def getFields(self):
+        return self.fields
