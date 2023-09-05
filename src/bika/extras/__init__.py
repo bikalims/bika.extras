4c82f2f9fd0fc94471a67a786fde110f28fc6efa
Lunga Baliwe <lunga001@gmail.com>
2023-09-05 10:26:17 +0200

    Start with moving reports

--- src/bika/extras/__init__.py
+++ src/bika/extras/__init__.py
@@ -3,8 +3,17 @@
 import logging
 from zope.i18nmessageid import MessageFactory
 
+from bika.lims.api import get_request
+from bika.extras.interfaces import IBikaExtrasLayer
+
 PRODUCT_NAME = "bika.extras"
 PROFILE_ID = "profile-{}:default".format(PRODUCT_NAME)
 logger = logging.getLogger(PRODUCT_NAME)
 
 _ = MessageFactory(PRODUCT_NAME)
+
+
+def is_installed():
+    """Returns whether the product is installed or not"""
+    request = get_request()
+    return IBikaExtrasLayer.providedBy(request)
