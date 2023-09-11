8282b6ef7e448b2019790fd156c75c529cf3a568
Lunga Baliwe <lunga001@gmail.com>
2023-09-11 12:06:03 +0200

    Custom listing taken from senaite.core

---
+++ src/bika/extras/browser/listingview/bika_instrumenttypes.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from zope.component import adapts
+from zope.interface import implements
+
+from bika.extras import is_installed
+from senaite.app.listing.interfaces import IListingView
+from senaite.app.listing.interfaces import IListingViewAdapter
+
+
+class InstrumentTypesListingViewAdapter(object):
+    adapts(IListingView)
+    implements(IListingViewAdapter)
+
+    def __init__(self, listing, context):
+        self.listing = listing
+        self.context = context
+
+    def before_render(self):
+        if not is_installed():
+            return
+        self.listing.contentFilter["sort_order"] = "ascending"
+
+    def folder_item(self, obj, item, index):
+        if not is_installed():
+            return item
+        return item
