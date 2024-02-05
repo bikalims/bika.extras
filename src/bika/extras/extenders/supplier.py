0260f2e8963f34dc5b76ee1e15d7890be6a5f811
Lunga Baliwe <lunga001@gmail.com>
2024-02-05 12:51:25 +0200

    LIMS-76

---
+++ src/bika/extras/extenders/supplier.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from Products.Archetypes.Widget import StringWidget
+from archetypes.schemaextender.interfaces import IBrowserLayerAwareExtender
+from archetypes.schemaextender.interfaces import ISchemaModifier
+from archetypes.schemaextender.interfaces import ISchemaExtender
+from zope.interface import implements
+from zope.component import adapts
+from zope.interface import implementer
+
+from bika.aquaculture.config import _
+from bika.aquaculture.config import is_installed
+from bika.aquaculture.interfaces import IBikaAquacultureLayer
+from bika.extras.extenders.fields import ExtStringField
+from bika.lims.interfaces import ISupplier
+
+
+lab_account_number = ExtStringField(
+    "LabAccountNumber",
+    mode="rw",
+    widget=StringWidget(
+        label=_(u"Lab Account Number"),
+        description=_("Lab's account number with a Supplier"),
+    ),
+)
+
+
+@implementer(ISchemaExtender, IBrowserLayerAwareExtender)
+class SupplierSchemaExtender(object):
+    adapts(ISupplier)
+    layer = IBikaAquacultureLayer
+
+    fields = [
+        lab_account_number,
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getOrder(self, schematas):
+        return schematas
+
+    def getFields(self):
+        return self.fields
+
+
+class SupplierSchemaModifier(object):
+    adapts(ISupplier)
+    implements(ISchemaModifier)
+    layer = IBikaAquacultureLayer
+
+    def __init__(self, context):
+        self.context = context
+
+    def fiddle(self, schema):
+        """
+        """
+        if is_installed():
+            schema['Website'].widget.label = "Website"
+            schema['SWIFTcode'].widget.label = "SWIFT code"
+
+        return schema
