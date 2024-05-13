19919574ca7024145a071d7485c973684573b57e
Lunga Baliwe <lunga001@gmail.com>
2024-05-13 19:12:33 +0200

    LIMS-908: Fixed send email when samples are created in received state

--- src/bika/extras/workflow/analysisrequest/__init__.py
+++ src/bika/extras/workflow/analysisrequest/__init__.py
@@ -6,10 +6,13 @@ import events
 def AfterTransitionEventHandler(sample, event):
     """Actions to be done *after a transition* for a sample takes place
     """
-    if not event.transition:
+    if not event.transition and event.status["action"] != "receive":
         return
 
-    function_name = "after_{}".format(event.transition.id)
+    if event.status["action"] == "receive":
+        function_name = "after_{}".format(event.status["action"])
+    else:
+        function_name = "after_{}".format(event.transition.id)
     if hasattr(events, function_name):
         # Call the after_* function from events package
         getattr(events, function_name)(sample)
