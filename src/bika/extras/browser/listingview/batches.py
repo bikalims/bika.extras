1588250da591dcd3de7593d541486265085619dc
Lunga Baliwe <lunga001@gmail.com>
2026-08-23 11:45:05 +0200

    Add instrument and creator listing columns

--- src/bika/extras/browser/listingview/batches.py
+++ src/bika/extras/browser/listingview/batches.py
@@ -25,11 +25,14 @@ class BatchesListingViewAdapter(object):
         num_samples = [
             ("getNumberOfSamples", {"toggle": False, "title": _("Samples")})
         ]
+        creator = [("Creator", {"toggle": False, "title": _("Creator")})]
         self.listing.columns.update(num_samples)
+        self.listing.columns.update(creator)
         for i in range(len(self.listing.review_states)):
             self.listing.review_states[i]["columns"].append(
                 "getNumberOfSamples"
             )
+            self.listing.review_states[i]["columns"].append("Creator")
 
     def folder_item(self, obj, item, index):
         if not is_installed():
@@ -37,4 +40,5 @@ class BatchesListingViewAdapter(object):
 
         full_object = api.get_object(obj)
         item["getNumberOfSamples"] = full_object.getNumberOfSamples()
+        item["Creator"] = api.get_user_fullname(full_object.Creator())
         return item
--- src/bika/extras/browser/listingview/worksheet.py
+++ src/bika/extras/browser/listingview/worksheet.py
@@ -3,6 +3,8 @@
 from zope.component import adapts
 from zope.interface import implements
 
+from bika.lims import api
+from bika.lims.utils import get_link_for
 from bika.extras import is_installed
 from bika.extras.config import _
 from senaite.app.listing.interfaces import IListingView
@@ -21,6 +23,13 @@ class WorksheetsListingViewAdapter(object):
         if not is_installed():
             return
 
+        instrument = [
+            ("Instrument", {"toggle": False, "title": _("Instrument")})
+        ]
+        self.listing.columns.update(instrument)
+        for review_state in self.listing.review_states:
+            review_state["columns"].append("Instrument")
+
         try:
             if not self.context.bika_setup.WorksheetTitle:
                 return
@@ -35,6 +44,13 @@ class WorksheetsListingViewAdapter(object):
     def folder_item(self, obj, item, index):
         if not is_installed():
             return item
+
+        worksheet = api.get_object(obj)
+        instrument = worksheet.getInstrument()
+        if instrument:
+            item["Instrument"] = api.get_title(instrument)
+            item["replace"]["Instrument"] = get_link_for(instrument)
+
         try:
             if not self.context.bika_setup.WorksheetTitle:
                 return item
